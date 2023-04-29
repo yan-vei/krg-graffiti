1e6e56f07f5ce94bbe5b8260430e4d2a37a39004
Yana Veitsman <yana.veitsman@yandex.kz>
2023-04-30 00:20:50 +0600

    add landing page

---
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent {
+  lat: number = 49.495990;
+  lng: number = 73.095688;
+
+}
