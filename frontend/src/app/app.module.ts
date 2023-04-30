40cf79e3eb856ff7c1c6a2e3e46982fe56b12837
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-04-30 17:04:14 +0600

    Basic layout for the frontend app (#7)
    * add landing page
    
    * add env files to .gitignore
    
    * add marker, routing, and zoom for the map page
    
    * remove .vscode folder
    
    * remove overriding gitignore
    
    * add about us page
    
    * add favicon
    
    * add send graffiti form
    
    * remove editor settings

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AgmCoreModule } from '@agm/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { environment } from 'src/environments/environment.prod';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { GraffitiSendingFormComponent } from './graffiti-sending-form/graffiti-sending-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LandingPageComponent,
+    AboutUsComponent,
+    GraffitiSendingFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AgmCoreModule.forRoot({
+      apiKey: environment.googleMapsApiKey
+    }),
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.css']
+})
+export class LandingPageComponent {
+  lat: number = 49.808571116626005;
+  lng: number = 73.10329162306904;
+}
