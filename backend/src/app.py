c8bda4e89cc781958bbbaf071dbbb5805e985a62
Yana Veitsman <yana.veitsman@yandex.kz>
2023-05-31 22:50:14 +0600

    setup image upload to the aws bucket

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
+from backend.src.routes import images
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+app.register_blueprint(images.images)
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
