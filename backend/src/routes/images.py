5d9f74e44503a363a202f600c424855eac827b0d
Yana Veitsman <yana.veitsman@yandex.kz>
2023-06-01 14:47:16 +0600

    send IOBytes directly from the formfile data

--- backend/src/routes/images.py
+++ backend/src/routes/images.py
@@ -1,6 +1,5 @@
 from flask import Blueprint
 from flask import make_response, request
-import base64
 from backend.src.utils import aws_helper
 
 
@@ -11,10 +10,9 @@ images = Blueprint("images", __name__)
 def upload_image_to_aws():
 
     image = request.files['image']
-    image_str = base64.b64encode(image.read())
 
     try:
-        image_url = aws_helper.upload_to_aws(image_str)
+        image_url = aws_helper.upload_to_aws(image)
         return make_response({"image_url": image_url}, 200)
     except:
         return make_response({"error": "Image failed to upload to AWS."}, 500)
--- backend/src/utils/aws_helper.py
+++ backend/src/utils/aws_helper.py
@@ -1,9 +1,8 @@
 import imghdr
-import io
 import uuid
+from io import BytesIO
 import boto3
 from backend.src import config
-import base64
 
 
 AWS_ACCESS_KEY = config.AWS_ACCESS_KEY
@@ -17,22 +16,10 @@ def get_file_extension(file_name, decoded_file):
     return extension
 
 
-def decode_base64_file(data):
-    try:
-        decoded_file = base64.b64decode(data)
-    except TypeError:
-        TypeError('invalid_image')
-
+def upload_to_aws(image_file):
+    file = BytesIO(image_file.read())
     file_name = str(uuid.uuid4())[:12]
-    file_extension = get_file_extension(file_name, decoded_file)
 
-    complete_file_name = "%s.%s" % (file_name, file_extension,)
-
-    return io.BytesIO(decoded_file), complete_file_name
-
-
-def upload_to_aws(base64_image):
-    file, file_name = decode_base64_file(base64_image)
     client = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
     client.upload_fileobj(
         file,
