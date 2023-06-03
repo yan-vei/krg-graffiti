07105de1e79a745252f70c23453899c06fce3f30
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-06-03 10:32:56 +0600

    Fix merging (#40)
    * Retrieving and patching graffiti images (#35)
    
    * finish graffiti sending endpoint (#31)
    
    * add endpoints for retrieving and patching images (#34)
    
    * make some images endpoints available only to admin (#39)

---
+++ backend/src/utils/decorators.py
@@ -0,0 +1,33 @@
+from functools import wraps
+from flask import make_response, request, jsonify
+from backend.src.config import ADMIN_TOKEN
+
+
+def is_admin(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        has_valid_admin_token = False
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return f(has_valid_admin_token, *args, **kwargs)
+        elif token != ADMIN_TOKEN:
+            return f(has_valid_admin_token, *args, **kwargs)
+        has_valid_admin_token = True
+        return f(has_valid_admin_token, *args, **kwargs)
+    return decorator
+
+
+def admin_rights_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+        if not token:
+            return make_response(jsonify({"error": "A valid token is missing."}), 401)
+        elif token != ADMIN_TOKEN:
+            return make_response(jsonify({"error": "Token is invalid."}), 403)
+        return f(*args, **kwargs)
+    return decorator
