c031bb9a7d4945afcffa122d8e1f297f92997d04
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-06-10 19:26:16 +0600

    Implement sending graffiti from frontend (#57)
    * patch the whole graffiti obj (#52) (#53)
    
    * set file and form
    
    * add validation of the form fields
    
    * fix initialzing the form
    
    * attaching validators to the form
    
    * send graffiti to the backend service

---
+++ frontend/src/app/entities/graffiti-form.model.ts
@@ -0,0 +1,9 @@
+export class GraffitiForm {
+  constructor(
+    public address: string,
+    public zip: string,
+    public latitude: string,
+    public longitude: string,
+    public comment: string
+  ) {}
+}
