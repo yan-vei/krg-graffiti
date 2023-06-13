5a5a37d727d44dbc575fac8a9cf8543bcbeffff9
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-06-13 11:27:38 +0600

    Lint on pre-commit (#75)
    * add pre-commit linting
    
    * configure pylint

--- backend/src/crud/graffiti.py
+++ backend/src/crud/graffiti.py
@@ -3,8 +3,9 @@ from backend.src.models.graffiti import Graffiti, GraffitiSchema
 
 
 def create_graffiti(image_url, address, longitude, latitude, zip, comment, has_admin_checked=False):
-    graffiti = Graffiti(image_url=image_url, address=address, longitude=longitude, latitude=latitude, zip=zip,
-                        comment=comment, has_admin_checked=has_admin_checked)
+    graffiti = Graffiti(image_url=image_url, address=address, longitude=longitude, 
+                        latitude=latitude, zip=zip, comment=comment, 
+                        has_admin_checked=has_admin_checked)
 
     session = Session()
     session.add(graffiti)
