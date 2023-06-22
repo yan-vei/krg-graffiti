bb1c0c3911257323c5e0a978854be63d6c90fa15
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-06-22 17:21:17 +0600

    configure db host in config (#88)

--- backend/src/models/entity.py
+++ backend/src/models/entity.py
@@ -3,7 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from src import config
 
-engine = create_engine(config.POSTGRES_DB_URL)
+db_url = f'{config.POSTGRES_TARGET}:5432'
+db_name = config.POSTGRES_DB
+db_user = config.POSTGRES_USER
+db_password = config.POSTGRES_PASSWORD
+
+engine = create_engine(f'postgresql://{db_user}:{db_password}@{db_url}/{db_name}')
+
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
