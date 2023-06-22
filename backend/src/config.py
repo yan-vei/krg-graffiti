0defa92b9fb399df7d0ba2deede5e9871dcf5fc9
Yana Veitsman <61601138+yan-vei@users.noreply.github.com>
2023-06-22 17:21:40 +0600

    configure db host in config (#88) (#89)

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,8 +1,15 @@
 from dotenv import load_dotenv
 import os
+from sys import platform
 
 load_dotenv()
 
+POSTGRES_TARGET = "localhost"
+if (container_name := os.environ.get('DB_CONTAINER_NAME')):
+    POSTGRES_TARGET = container_name
+POSTGRES_DB = os.environ.get('POSTGRES_DB')
+POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+POSTGRES_USER = os.environ.get('POSTGRES_USER')
 AWS_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
